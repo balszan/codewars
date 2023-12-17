8a725d49e2dba5ca29c42380e7f821057ad75164
balszan <kbalszan@gmail.com>
2023-12-17 14:52:52 +0000

    Solution

--- 6kyu/find-odd-int.js
+++ 6kyu/find-odd-int.js
@@ -1,4 +1,21 @@
 function findOdd(A) {
-    //happy coding!
-    return 0;
+    let occurrences = new Map()
+    A.forEach((number) => {
+        if(occurrences.get(number)==undefined) {
+            occurrences.set(number, 1)
+        } else {
+            occurrences.set(number, occurrences.get(number)+1)
+        }      
+    })
+
+    let result
+
+      occurrences.forEach((value,key) => {
+        if((value)%2!=0) {
+            result = key
+        }
+    })
+    
+    return result 
+
   }
