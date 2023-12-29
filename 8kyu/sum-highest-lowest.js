6108e1ec429927cb45681020086bac03e31ba902
balszan <kbalszan@gmail.com>
2023-12-29 09:11:16 +0000

    Solution

---
+++ 8kyu/sum-highest-lowest.js
@@ -0,0 +1,11 @@
+function sumArray(array) {
+    if(array == null || array.length<2) {
+        return 0
+    } else {
+        let newArray = array.sort((a,b) => a-b)
+        newArray.splice(0,1)
+        newArray.splice(newArray.length-1,1)
+        return newArray.reduce((acc,c) => acc+c,0)
+    }
+    
+}
