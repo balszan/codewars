d4bbdcacdf2756e8166ef8519ab7fd285f5d969c
balszan <kbalszan@gmail.com>
2024-01-02 09:33:02 +0000

    Solution

--- 8kyu/count-by-x.js
+++ 8kyu/count-by-x.js
@@ -1,15 +1,7 @@
 function countBy(x, n) {
     let result = []
-    
     for(i=1;i<=n;i++) {
         result.push(i*x)
-        alert(result)
     }
-
     return result
-    
-
   }
-
-  countBy(1,10)
-  countBy(2,5)
