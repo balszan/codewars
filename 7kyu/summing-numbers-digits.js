5662c45883df4ac1335bfb51db1f055c873281a1
balszan <kbalszan@gmail.com>
2024-01-27 09:03:06 +0000

    Updated solution

--- 7kyu/summing-numbers-digits.js
+++ 7kyu/summing-numbers-digits.js
@@ -1,6 +1,5 @@
 function sumDigits(number) {
-    let strArr = String(number).split("")
+    let strArr = String(Math.abs(number)).split("")
     let numArr = strArr.map((num) => Number(num))
     return numArr.reduce((acc,c) => +acc+ +c,0)
 }
-console.log(sumDigits(-32))
