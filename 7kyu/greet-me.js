206801bc455e2665708020fa89988c66587b40d5
balszan <kbalszan@gmail.com>
2024-05-18 11:04:11 +0100

    Solution

--- 7kyu/greet-me.js
+++ 7kyu/greet-me.js
@@ -1,4 +1,4 @@
 var greet = function(name) {
-    let CapitalisedName = name.charAt(0).toUpperCase()+name.slice(1)
+    let CapitalisedName = name.charAt(0).toUpperCase()+name.slice(1).toLowerCase()
     return "Hello " + CapitalisedName + "!"
 }
